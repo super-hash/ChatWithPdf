02573202ad88a45ced04329e868668c7f0b552a4
step by step <73412586+super-hash@users.noreply.github.com>
2023-07-31 15:09:37 +0800

    Update pdfquery.py

--- pdfquery.py
+++ pdfquery.py
@@ -2,57 +2,34 @@ import os
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
-from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import PyPDFium2Loader
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
+# from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+
 
 class PDFQuery:
-    def __init__(self, openai_api_key=None):
-        """
-        Initializes the PDFQuery object.
-
-        Args:
-        openai_api_key (str): The API key for OpenAI.
-        """
-        self.embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-        os.environ["OPENAI_API_KEY"] = openai_api_key
-        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-        self.llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+    def __init__(self):
+        os.environ["OPENAI_API_KEY"] = "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"
+        self.embeddings = OpenAIEmbeddings()
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=200)
+        # self.llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+        self.llm = ChatOpenAI(temperature=0)
         self.chain = None
         self.db = None
 
-    def ask(self, question):
-        """
-        Asks a question from the ingested document.
-
-        Args:
-        question (str): The question to be asked.
-
-        Returns:
-        str: The response to the question.
-        """
+    def ask(self, question: str) -> str:
         if self.chain is None:
-            return "Please, add a document."
-        
-        docs = self.db.get_relevant_documents(question)
-        return self.chain.run(input_documents=docs, question=question)
+            response = "Please, add a document."
+        else:
+            docs = self.db.get_relevant_documents(question)
+            response = self.chain.run(input_documents=docs, question=question)
+        return response
 
-    def ingest(self, file_path):
-        """
-        Ingests a document from a file path.
-
-        Args:
-        file_path (str): The path to the file.
-        """
-        loader = PyPDFLoader(file_path)
+    def ingest(self, file_path: os.PathLike) -> None:
+        loader = PyPDFium2Loader(file_path)
         documents = loader.load()
         splitted_documents = self.text_splitter.split_documents(documents)
         self.db = Chroma.from_documents(splitted_documents, self.embeddings).as_retriever()
-        self.chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
-
-    def forget(self):
-        """
-        Removes the ingested documents and resets the chain.
-        """
-        self.db = None
-        self.chain = None
+        # self.chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
+        self.chain = load_qa_chain(ChatOpenAI(temperature=0), chain_type="stuff")
